0493e83d3a86e439abb7140d6749e83b511b192d
JLTambley <jorgetambleygomez@gmail.com>
2025-10-08 18:22:24 -0400

    123

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"]
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="x-default">
       <head>
         {/* ✅ Organization Schema */}
         <script
